71595224d60e9e0f28da06e64d2411bf09042c59
Nalindaka Hettiarachchi <nalindakasandeep@gmail.com>
2023-12-04 14:24:55 +1000

    Add shoing questions and selecting answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -7,9 +7,24 @@ export default function Quiz() {
 
   const activeQuestionIndex = userAnswers.length;
 
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevState) => {
+      return [...prevState, selectedAnswer];
+    });
+  }
+
   return (
-    <div id="question">
-      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer()}>{answer}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
